1b53318d7fa84f8ef76f67554a89686075f22858
Akmal Sindarov <asauz777@gmail.com>
2025-01-02 11:20:11 +1000

    framework-goFiber/go.mod

---
+++ framework-goFiber/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+)
+
+func main() {
+	// Create a new Fiber app
+	app := fiber.New()
+
+	// Set up the logging middleware
+	app.Use(logger.New())
+
+	// Define a simple route
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World! 🌍")
+	})
+
+	// Start the server
+	if err := app.Listen(":3000"); err != nil {
+		panic(err)
+	}
+}
